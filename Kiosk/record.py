c606b9834ac54cddff1394c4ccb77d8adf105740
james-dewes <james@jamesdewes.com>
2016-10-29 20:27:26 +0100

    added gzip

--- Kiosk/record.py
+++ Kiosk/record.py
@@ -4,9 +4,11 @@ except RuntimeError:
     print("unable to import capture")
 
 import time
+import gzip
 
 camera = capture()
 camera.start_capture()
 time.sleep(30)
 camera.stop_capture()
-print("capture complete")
+print("capture complete. Zipping")
+gzip.GzipFile('example.h264')

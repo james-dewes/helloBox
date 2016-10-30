a7af96e573602469e24b759f01f5044d64c46b14
JakeArnoldSummit <jake.arnold@summitmedia.com>
2016-10-30 09:19:10 +0000

    Added audio capture to video capture/store

--- Kiosk/record.py
+++ Kiosk/record.py
@@ -7,8 +7,10 @@ import time
 import gzip
 import json
 import os
+import subprocess
 
 camera = capture()
+subprocess.call('arecord -d 10 -c 1 -D plughw:1,0 -v -r 48000 -t wav -f S16_LE -M audio.wav')
 camera.start_capture()
 time.sleep(10)
 camera.stop_capture()

1c6526123598344380d75f82a687e348a1c01fd6
james-dewes <james@jamesdewes.com>
2016-10-30 12:01:18 +0000

    aasd

--- Kiosk/record.py
+++ Kiosk/record.py
@@ -8,19 +8,10 @@ import gzip
 import json
 import os
 import subprocess
-from multiprocessing import Process
 
 camera = capture()
-def func1():
-    os.system('arecord -d 10 -c 1 -D plughw:1,0 -v -r 48000 -t wav -f S16_LE -M audio.wav', shell=True)
-
-def func2():
-    camera.start_capture()
-
-p1 = Process(target = func1)
-p1.start()
-p2 = Process(target = func2)
-p2.start()
+camera.start_capture()
+subprocess.call('arecord -d 10 -c 1 -D plughw:1,0 -v -r 48000 -t wav -f S16_LE -M audio.wav', shell=True)
 time.sleep(10)
 camera.stop_capture()
 ##print("capture complete. Zipping")

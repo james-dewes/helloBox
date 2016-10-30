0d6193729da45ced678dd4f2cc5ed437d68be821
james-dewes <james@jamesdewes.com>
2016-10-30 01:02:26 +0100

    permission change

--- Kiosk/dataHandler/handler.php
+++ Kiosk/dataHandler/handler.php
@@ -12,26 +12,24 @@ if($_GET['k']=='12345'){
 // This is the data you want to pass to Python
 //not right now, but might need this later
 //$data = array('as', 'df', 'gh');
-while(True){
-  // Execute the python script with the JSON data
-  $result = shell_exec('python ../triggers/button.py ' . escapeshellarg(json_encode($data)));
+// Execute the python script with the JSON data
+$result = shell_exec('python ../triggers/button.py ' . escapeshellarg(json_encode($data)));
 
-  // Decode the result
-  $resultData = json_decode($result, true);
-  //see if the button has been pressed
-  if($resultData['status'] == 'True'){
-    shell_exec('../record.py');
-    //send the file to the server
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "http://192.168.226.240/insertIntoDB.php");
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_exec($ch);
+// Decode the result
+$resultData = json_decode($result, true);
+//see if the button has been pressed
+if($resultData['status'] == 'True'){
+  shell_exec('sudo ../record.py');
+  //send the file to the server
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, "http://192.168.226.240/insertIntoDB.php");
+  curl_setopt($ch, CURLOPT_HEADER, 0);
+  curl_exec($ch);
 
 // close cURL resource, and free up system resources
 curl_close($ch);
   }
 sleep(5);
-}
 
 
 

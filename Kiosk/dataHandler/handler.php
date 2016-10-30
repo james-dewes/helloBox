a7ba67143d1b32c997ca9cf2325140678114d19c
james-dewes <james@jamesdewes.com>
2016-10-30 04:07:24 +0000

    video not videos

--- Kiosk/dataHandler/handler.php
+++ Kiosk/dataHandler/handler.php
@@ -48,8 +48,8 @@ if($resultData['status'] == 'True'){
   //close the return file
 
   //get return video and audio
-  $videoLocation = "wget http://192.168.226.240/videos/{$incomingFileName}.h264";
-  $audioLocation = "wget http://192.168.226.240/videos/{$incomingFileName}";
+  $videoLocation = "wget http://192.168.226.240/video/{$incomingFileName}.h264";
+  $audioLocation = "wget http://192.168.226.240/audio/{$incomingFileName}";
   exec($videoLocation);
   //exec($audioLocation);
   echo json_encode(array('files'=>'ready'));

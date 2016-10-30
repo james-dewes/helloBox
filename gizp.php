dbfae2ab9ebb054192bb87cd2fad7c78a6765466
james-dewes <james@jamesdewes.com>
2016-10-30 09:36:20 +0000

    remove

--- gizp.php
+++ gizp.php
@@ -34,23 +34,23 @@ function gzCompressFile($source, $level = 9){
 //from elsewhere on stack stackoverflow.com/questions/11265914/how-can-i-extract-or-uncompress-gzip-file-using-php
 function gzInflateFile($file_name){
   // Raising this value may increase performance
-  $buffer_size = 4096; // read 4kb at a time
-  $out_file_name = str_replace('.gz', '', $file_name);
+  //$buffer_size = 4096; // read 4kb at a time
+  //$out_file_name = str_replace('.gz', '', $file_name);
 
   // Open our files (in binary mode)
-  $file = gzopen($file_name, 'rb');
-  $out_file = fopen($out_file_name, 'wb');
+//  $file = gzopen($file_name, 'rb');
+  //$out_file = fopen($out_file_name, 'wb');
 
   // Keep repeating until the end of the input file
-  while (!gzeof($file)) {
+  //while (!gzeof($file)) {
       // Read buffer-size bytes
       // Both fwrite and gzread and binary-safe
-      fwrite($out_file, gzread($file, $buffer_size));
+    //  fwrite($out_file, gzread($file, $buffer_size));
   }
 
   // Files are done, close files
-  fclose($out_file);
-  gzclose($file);
+  //fclose($out_file);
+  //gzclose($file);
 
 }
 ?>

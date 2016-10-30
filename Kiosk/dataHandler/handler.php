8bbc4b2dfa78e32d6a1f854db19fc2855ed4aa7a
james-dewes <james@jamesdewes.com>
2016-10-30 04:00:26 +0000

    wget not wegt

--- Kiosk/dataHandler/handler.php
+++ Kiosk/dataHandler/handler.php
@@ -48,7 +48,7 @@ if($resultData['status'] == 'True'){
   //close the return file
 
   //get return video and audio
-  $videoLocation = "wegt http://192.168.226.240/videos/{$incomingFileName}.h264";
+  $videoLocation = "wget http://192.168.226.240/videos/{$incomingFileName}.h264";
   $audioLocation = "wget http://192.168.226.240/videos/{$incomingFileName}";
   exec($videoLocation);
   //exec($audioLocation);
